721c8b005aedd3816f3eda4a287b91abc7e3d87b
Carlos Quesada Umaña <carlosquesadaumana@gmail.com>
2024-04-03 16:28:51 -0400

    Exercises fetched and prev steps form button added to create routine page

--- app/(modules)/routines/(infrastucture)/(pages)/create/page.tsx
+++ app/(modules)/routines/(infrastucture)/(pages)/create/page.tsx
@@ -1,6 +1,10 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import useStepperForm from '@/app/(hooks)/useStepperForm'
+import ExercisesSelection from './components/FormSteps/ExercisesSelection/ExercisesSelection'
+import DaysFrequencySelection from './components/FormSteps/DaysFrequencySelection/DaysFrequencySelection'
+import { supabase } from '@/app/(config)/supabase'
+import { groupExercisesList } from '../../utils/utils'
 
 enum CREATE_ROUTINE_STEPS {
     EXERCISES_SELECTION = 0,
@@ -8,40 +12,62 @@ enum CREATE_ROUTINE_STEPS {
 }
 
 const page = () => {
+    const [groupedExercisesList, setGroupedExercisesList] = useState<any[]>([])
 
     const {
-        step, 
-        nextStep, 
+        step,
+        nextStep,
         prevStep
     } = useStepperForm()
 
-  return (
-    <div>
-        <h2>CREATE ROUTINE FORM</h2>
+    const handleGetExercisesData = async () => {
+        const { data } = await supabase.from('Exercises').select()
 
-        {
-            step === CREATE_ROUTINE_STEPS.EXERCISES_SELECTION && (
-                <div>
-                    <h3>EXERCISES SELECTION</h3>
-                    <p>Here you will select the exercises for your routine</p>
-                </div>
-            )
-        }
+        const groupedExercises = groupExercisesList(data)
 
-        {
-            step === CREATE_ROUTINE_STEPS.DAYS_FREQUENCY_SELECTION && (
-                <div>
-                    <h3>DAYS FREQUENCY SELECTION</h3>
-                    <p>Here you will select the days for your routine</p>
-                </div>
-            )
-        }
+        setGroupedExercisesList(Object.entries(groupedExercises || {}))
+    }
 
-        <button
-            onClick={nextStep}
-        >next</button>
-    </div>
-  )
+    const handleData = async () => {
+        handleGetExercisesData()
+    }
+
+    useEffect(() => {
+        handleData()
+    }, [])
+
+    return (
+        <div>
+            <h2>CREATE ROUTINE FORM</h2> 
+
+            {
+                step === CREATE_ROUTINE_STEPS.EXERCISES_SELECTION && (
+                    <ExercisesSelection 
+                        groupedExercisesList={groupedExercisesList}
+                    />
+                )
+            }
+
+            {
+                step === CREATE_ROUTINE_STEPS.DAYS_FREQUENCY_SELECTION && (
+                    <DaysFrequencySelection />
+                )
+            }
+
+            {
+                step > CREATE_ROUTINE_STEPS.EXERCISES_SELECTION && (
+                    <button
+                        className='mr-2'
+                        onClick={prevStep}
+                    >back</button>
+                )
+            }
+
+            <button
+                onClick={nextStep}
+            >next</button>
+        </div>
+    )
 }
 
 export default page
