fde88354f81d50d36d5f74c36b9ae4d33fc12134
Carlos Quesada Umaña <carlosquesadaumana@gmail.com>
2024-04-03 16:29:33 -0400

    groupedExercisesList rendered on ExercisesSelection

--- app/(modules)/routines/(infrastucture)/(pages)/create/components/FormSteps/ExercisesSelection/ExercisesSelection.tsx
+++ app/(modules)/routines/(infrastucture)/(pages)/create/components/FormSteps/ExercisesSelection/ExercisesSelection.tsx
@@ -2,36 +2,45 @@
 import { DEFAULT_CARDS_STYLES } from '@/app/(styles)/variables'
 import React, { useState } from 'react'
 
-const ExercisesSelection = () => {
+interface ExercisesSelectionProps {
+    groupedExercisesList: any
+}
+
+const ExercisesSelection = ({ groupedExercisesList }: ExercisesSelectionProps) => {
 
     const [selectedExercisesList, setSelectedExercisesList] = useState<any[]>([])
-
-    const list = [
-        "Bench press",
-        "Incline bench press",
-        "Biceps curl",
-        "Triceps extension",
-    ]
 
     const handleSelectExercise = (exercise: string) => {
         setSelectedExercisesList([...selectedExercisesList, exercise])
     }
 
-    console.log({ selectedExercisesList })
-
     return (
-        <div className='grid gap-4 sm:grid-cols-2 sm:gap-4'>
+        <div
+            className='grid gap-4 sm:grid-cols-2 sm:gap-4'
+        >
             <div>
                 <h2 className='mb-3'>ExercisesSelection</h2>
-                <div className='grid gap-4 sm:grid-cols-2 sm:gap-4'>
+                <div
+                    // className='grid gap-4 sm:grid-cols-2 sm:gap-4'
+                >
                     {
-                        list.map((exercise, index) => (
-                            <div key={index}
-                                className={`${DEFAULT_CARDS_STYLES}`}
-                                onClick={() => handleSelectExercise(exercise)}
-                            >{exercise}</div>
-                        ))
-                    }
+                        groupedExercisesList
+                            .map(([category, exercises]: any[], index: any) => (
+                                <div key={index}>
+                                    <h2 className='my-2 font-bold capitalize'>{category}</h2>
+                                    <div className=''>
+                                        {exercises.map((exercise: any, index: any) => (
+                                            <div key={index}
+                                                className={`${DEFAULT_CARDS_STYLES} mt-4`}
+                                                onClick={() => handleSelectExercise(exercise)}
+                                            >
+                                                {/* Render the exercise data here */}
+                                                <p>{exercise.name}</p>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))}
                 </div></div>
             <div>
                 <h3 className='mb-3'>Selected</h3>
@@ -41,7 +50,7 @@ const ExercisesSelection = () => {
                             ? selectedExercisesList.map((exercise, index) => (
                                 <div key={index}
                                     className={`${DEFAULT_CARDS_STYLES}`}
-                                >{exercise}</div>
+                                >{exercise.name}</div>
                             ))
                             : <h3>No exercises selected</h3>
                     }
