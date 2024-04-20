82dbf46d576a208c842176465227eaec2da3168e
Carlos Quesada Umaña <carlosquesadaumana@gmail.com>
2024-04-20 07:35:36 -0500

    Some grid styles fixed

--- app/(modules)/routines/(infrastucture)/(pages)/[id]/session/[sessionId]/view/page.tsx
+++ app/(modules)/routines/(infrastucture)/(pages)/[id]/session/[sessionId]/view/page.tsx
@@ -16,21 +16,21 @@ const Page = ({ params }: any) => {
 
     return (
         <div>
-            <h1 className={`${HEADINGS.H1} font-bold text-center sm:text-left mb-6`}>{formatDate(currentWorkoutSession?.date)}</h1>
+            <h1 className={`${HEADINGS.H1} font-bold text-center md:text-left mb-6`}>{formatDate(currentWorkoutSession?.date)}</h1>
 
-            <div className='grid gap-4 sm:grid-cols-2 lg:grid-cols-3 sm:gap-6 text-center'>
+            <div className='grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:gap-6 text-center'>
                 {currentWorkoutSession !== undefined && currentWorkoutSession.exercises.map((exercise) => (
                     <div key={exercise.id} className='flex flex-col mb-10'>
-                        <h2 className={`${HEADINGS.H2} font-bold mb-6 sm:text-left`}>{exercise.name}</h2>
+                        <h2 className={`${HEADINGS.H2} font-bold mb-6 md:text-left`}>{exercise.name}</h2>
 
                         {/* <div 
                             className='w-auto '
                         > */}
-                            <h3 className='text-center sm:text-left font-bold text-[28px] mb-3'>Sets</h3>
+                            <h3 className='text-center md:text-left font-bold text-[28px] mb-3'>Sets</h3>
 
                             <ul>
                                 {exercise.sets.map((set, index) => (
-                                    <li key={index} className='mb-3 text-[22px] sm:text-left'>
+                                    <li key={index} className='mb-3 text-[22px] md:text-left'>
                                         <span className='font-bold'>{set.number}:</span> {set.formValues.reps} reps x {set.formValues.weight}{set.formValues.weightUnit} - {set.formValues.restTimeInMinutes} min
                                     </li>
                                 ))}
